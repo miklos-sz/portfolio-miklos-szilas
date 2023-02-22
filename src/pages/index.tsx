d515bbfbb148bebf1770b9afef6b7cc86ba215e4
miklos-sz <szilas.miklos@gmail.com>
2023-02-22 13:10:38 +0100

    add basic styling structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,8 +22,29 @@ const Home = ({ page: { title } }: HomeProps) => {
     <>
       <div>
         {title && (
-          <div>
-            <h1>{title}</h1>
+          <div
+            style={{
+              display: 'grid',
+              gap: '1rem',
+            }}
+          >
+            <p>Styling test</p>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+              atque excepturi commodi iste, possimus nostrum deleniti sit
+              officiis molestias debitis ut nemo facere non iure vitae ratione
+              sunt natus veniam?
+            </p>
+            <p className="lead">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+              atque excepturi commodi iste, possimus nostrum deleniti sit
+              officiis molestias debitis ut nemo facere non iure vitae ratione
+              sunt natus veniam?
+            </p>
           </div>
         )}
       </div>
